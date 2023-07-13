4b628fcfb068a39d5195ddbede28c7db2a159bdb
poppymint016 <phannita016@gmail.com>
2023-07-13 16:03:11 +0700

    add schema Portfolio
    - link
    - education
    - experience

---
+++ Server/src/Entity/experience.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Portfolio } from './Portfolio';
+
+@Entity()
+export class Experience{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    experience: string;
+
+    @CreateDateColumn({
+        type: "timestamp"
+    })
+    date: Date;
+
+    @ManyToOne(() => Portfolio, portfolio => portfolio.experience,{onDelete: 'SET NULL'})
+    portfolio: Portfolio[];
+}
