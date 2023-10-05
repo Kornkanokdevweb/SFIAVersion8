27a7fefbe2cefa61665c8d4035e011a611a8dda9
poppymint016 <phannita016@gmail.com>
2023-10-05 15:37:23 +0700

    Post information
    Detail-standards Page

--- client/src/app/pages/detail-standard/detail-standard.component.ts
+++ client/src/app/pages/detail-standard/detail-standard.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { Emitter } from 'src/app/emitters/emitter';
-import { ActivatedRoute, Router  } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+interface Information {
+  info_id: number;
+  info_text: string;
+}
 
 @Component({
   selector: 'app-detail-standard',
@@ -18,33 +24,46 @@ export class DetailStandardComponent implements OnInit {
   skillDetails: any;
 
   selectedMetal: string | null = null;
-  selectedLevelDescriptions: string[] = [];
+  selectedLevelDescriptions: { description_text: string, descid: string }[] = [];
+  selectedDescriptionIndex: number | null = null;
+
+  descid: string[] = [];
 
   visible: boolean[] = [];
 
   isLoggedIn: boolean = false;
   message = '';
 
+  information: Information[] = [];
+  updateForm: FormGroup;
+
   constructor(
     private messageService: MessageService,
     private route: ActivatedRoute,
     private http: HttpClient,
+    private formBuilder: FormBuilder,
     private router: Router
-  ) { }
+  ) {
+    this.updateForm = this.formBuilder.group({
+      info_id: '',
+      info_text: '',
+      descid: '',
+    });
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.codeskill = params['codeskill']; // Get the skill code from route parameters
       this.fetchSkillDetails();
       this.http.get('http://localhost:8080/api/user')
-      .subscribe({
-        next: (res: any) => {
-          Emitter.authEmitter.emit(true)
-        },
-        error: () => {
-           // ตั้งค่า URL ของหน้าล็อกอินตามที่คุณต้องการ
-        }
-      });
+        .subscribe({
+          next: (res: any) => {
+            Emitter.authEmitter.emit(true)
+          },
+          error: () => {
+            // ตั้งค่า URL ของหน้าล็อกอินตามที่คุณต้องการ
+          }
+        });
     });
   }
 
@@ -58,47 +77,6 @@ export class DetailStandardComponent implements OnInit {
           this.router.navigate(['/login']); // ตั้งค่า URL ของหน้าล็อกอินตามที่คุณต้องการ
         }
       });
-  }
-
-  showDialog(index: number) {
-    this.visible[index] = true;
-    this.checkLoginStatus();
-  }
-
-  showDetails(levelName: string) {
-    this.http.get(`http://localhost:8080/api/search?codeskill=${this.codeskill}&level_name=${levelName}`)
-      .subscribe(
-        (details: any) => {
-          this.selectedMetal = levelName;
-          const selectedLevels = details[0]?.levels.filter((level: any) => level.level_name === levelName);
-
-          if (selectedLevels && selectedLevels.length > 0) {
-            // สร้างอาร์เรย์ของคำอธิบายของทุก level ที่เกี่ยวข้อง
-            const descriptions = selectedLevels.map((level: any) => level.descriptions[0]?.description_text || "");
-            // รวมข้อมูลใน descriptions และเขียนเป็นข้อความที่สามารถแสดงได้ใน HTML
-            this.selectedLevelDescriptions = descriptions;
-
-            // อัพเดตความยาวของอาร์เรย์ visible
-            this.visible = new Array(descriptions.length).fill(false);
-          } else {
-            this.selectedLevelDescriptions = ["Level not found"];
-          }
-        },
-        (error) => {
-          console.error('Error fetching skill details:', error);
-        }
-      );
-  }
-
-  closeAddLink(index: number) {
-    this.visible[index] = false;
-  }
-
-  saveAddLink(index: number) {
-    // ทำการบันทึกข้อมูลที่แก้ไข
-    this.visible[index] = false;
-    // เมื่อบันทึกเสร็จแล้วให้ปิดหน้าต่าง
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Add data successfully' });
   }
 
   fetchSkillDetails() {
@@ -117,6 +95,89 @@ export class DetailStandardComponent implements OnInit {
           console.error('Error fetching skill details:', error);
         }
       );
+  }
+
+  showDetails(levelName: string) {
+    this.http.get(`http://localhost:8080/api/search?codeskill=${this.codeskill}&level_name=${levelName}`)
+      .subscribe(
+        (details: any) => {
+          this.selectedMetal = levelName;
+          const selectedLevels = details[0]?.levels.filter((level: any) => level.level_name === levelName);
+
+          if (selectedLevels && selectedLevels.length > 0) {
+            // สร้างอาร์เรย์ของคำอธิบายของทุก level ที่เกี่ยวข้อง
+            const descriptions = selectedLevels.map((level: any) => {
+              const description_text = level.descriptions[0]?.description_text || "";
+              const descid = level.descriptions[0]?.id || "";
+              return { description_text, descid };
+            });
+            // รวมข้อมูลใน descriptions และเขียนเป็นข้อความที่สามารถแสดงได้ใน HTML
+            this.selectedLevelDescriptions = descriptions;
+            console.log(this.selectedLevelDescriptions);
+          } else {
+            this.selectedLevelDescriptions = [{ description_text: "Level not found", descid: "" }];
+          }
+        },
+        (error) => {
+          console.error('Error fetching skill details:', error);
+        }
+      );
+  }
+
+  closeAddLink(index: any) {
+    if (this.selectedDescriptionIndex !== null) { // เพิ่มการตรวจสอบนี้
+      this.visible[this.selectedDescriptionIndex] = false;
+    } else {
+      // จัดการกรณีที่ this.selectedDescriptionIndex มีค่าเป็น null
+      // อาจจะแสดงข้อความแจ้งเตือนหรือทำอย่างอื่นตามที่คุณต้องการ
+    }
+  }
+
+  displayAddInfo: boolean = false;
+
+  showDialog(i: number) {
+    this.visible[i] = true;
+    this.checkLoginStatus();
+    this.displayAddInfo = true;
+    this.selectedDescriptionIndex = i; // เก็บ index ของรายการที่เลือก
+    this.updateForm.reset({
+      info_text: '', // เมื่อแสดงป๊อปอัพให้รีเซ็ตค่าในฟอร์ม
+    });
+    const selectedDescription = this.selectedLevelDescriptions[i];
+    console.log("Selected description_text:", selectedDescription.description_text);
+    console.log("Selected descid:", selectedDescription.descid);
+  }
+
+  saveAddLink() {
+    if (this.selectedDescriptionIndex !== null) { // เพิ่มการตรวจสอบนี้
+      const formData = this.updateForm.value;
+      const descid = this.selectedLevelDescriptions[this.selectedDescriptionIndex].descid; // ใช้ descid จาก selectedDescriptionIndex
+      this.http.post(`http://localhost:8080/api/createDatacollection`, formData, {
+        params: { descriptionId: descid }, // ใช้ descid จาก selectedDescriptionIndex
+        withCredentials: true,
+      }).subscribe({
+        next: (res) => {
+          console.log('Information created successfully:', res);
+          this.fetchSkillDetails();
+          this.displayAddInfo = false;
+          this.updateForm.reset({
+            info_text: '',
+          });
+        },
+        error: (err) => {
+          console.error('Error creating Information:', err);
+        },
+      });
+
+      // Close the dialog and reset the form
+      this.visible[this.selectedDescriptionIndex] = false;
+
+      // Show a success message
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Add data successfully' });
+    } else {
+      // จัดการกรณีที่ this.selectedDescriptionIndex มีค่าเป็น null
+      // อาจจะแสดงข้อความแจ้งเตือนหรือทำอย่างอื่นตามที่คุณต้องการ
+    }
   }
 
   private getUniqueItems(array: string[]): string[] {
