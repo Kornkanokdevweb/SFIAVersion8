d336679189d6fda1f43b0fa73157a5160201f84c
Kornkanokdevweb <Kornkanok.dev@gmail.com>
2023-10-24 14:24:13 +0700

    fix bug

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NgApexchartsModule } from 'ng-apexcharts';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Component
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './layouts/toolbar/toolbar.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
@@ -23,26 +24,6 @@ import { DownloadComponent } from './pages/download/download.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HistoryComponent } from './pages/history/history.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component'
-
-import { ButtonModule } from 'primeng/button';
-import { DropdownModule } from 'primeng/dropdown';
-import { ToastModule } from 'primeng/toast';
-import { CardModule } from 'primeng/card';
-import { DialogModule } from 'primeng/dialog';
-import { PasswordModule } from 'primeng/password';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { PaginatorModule } from 'primeng/paginator';
-import { ChartModule } from 'primeng/chart';
-import { InputTextModule } from 'primeng/inputtext';
-import { CalendarModule } from 'primeng/calendar';
-import { ConfirmPopupModule } from 'primeng/confirmpopup';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MessagesModule } from 'primeng/messages';
-import { SlideMenuModule } from 'primeng/slidemenu';
-import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { ConfirmDialogComponent } from './layouts/confirm-dialog/confirm-dialog.component';
 import { RecoveryPasswordComponent } from './pages/recovery-password/recovery-password.component';
 import { ResetComponent } from './pages/reset/reset.component';
@@ -50,10 +31,24 @@ import { EducationComponent } from './layouts/education/education.component';
 import { ExperienceComponent } from './layouts/experience/experience.component';
 import { LinkComponent } from './layouts/link/link.component';
 import { JobComponent } from './layouts/job/job.component';
-import {MatMenuModule} from '@angular/material/menu';
 import { PortfolioInformationComponent } from './pages/portfolio-information/portfolio-information.component';
 import { SpiderchartComponent } from './layouts/spiderchart/spiderchart.component';
 import { DatachartComponent } from './layouts/datachart/datachart.component';
+
+// Module
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MessagesModule } from 'primeng/messages';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
@@ -95,19 +90,13 @@ import { DatachartComponent } from './layouts/datachart/datachart.component';
     FormsModule,
     BrowserAnimationsModule,
     ToastModule,
-    CardModule,
     DialogModule,
-    PasswordModule,
     ConfirmDialogModule,
-    PaginatorModule,
-    ChartModule,
     ReactiveFormsModule,
     InputTextModule,
-    CalendarModule,
     ConfirmPopupModule,
     MatDialogModule,
     MessagesModule,
-    SlideMenuModule,
     NgbPaginationModule,
     MatMenuModule,
     NgxPaginationModule,
